bc6b547688f09a5560667da2dda1691dc98c8cbd
ilhamrafif20 <ilham.rafif20@gmail.com>
2026-02-24 22:17:28 +0700

    fixing checkbox case

--- demoqa/demoqa.spec.js
+++ demoqa/demoqa.spec.js
@@ -7,7 +7,7 @@ test("demoqa - Text Box - Positive Case",async function ({page}) {
     await page.locator('//*[@id="permanentAddress"]').fill('Oslo, Norway',{delay:4000}) // fill permanent address
     await page.locator('//*[@id="submit"]').click() // click submit button
     expect(await page.getByText('Ilham')).toBeTruthy() // validation check if text true (appear)
-    await page.waitForTimeout(3000) 
+    // await page.waitForTimeout(3000) 
 })
 
 test("demoqa - Text Box - Negative Case",async function ({page}) {
@@ -16,17 +16,18 @@ test("demoqa - Text Box - Negative Case",async function ({page}) {
     await page.getByPlaceholder('name@example.com').fill('ilham.mail.com')
     await page.getByRole('button',{id:'submit'}).click()
     expect(page.getByPlaceholder('name@example.com')).toContainClass('mr-sm-2 field-error form-control') // validation for email field
-    await page.waitForTimeout(3000)
+    // await page.waitForTimeout(3000)
 })
 
-test.only("demoqa - Checkbox",async function ({page}) {
+test("demoqa - Checkbox",async function ({page}) {
     await page.goto('https://demoqa.com/')
     await page.getByText('Elements').click()
     await page.getByText('Check Box').click()
-    await page.locator('//*[@id="root"]/div[1]/div/div/div[2]/div[2]/div/div[3]/div/div/div/div/span[2]').click()
-    await page.locator('//*[@id="root"]/div[1]/div/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/span[2]').click()
-    await page.locator('//*[@id="root"]/div[1]/div/div/div[2]/div[2]/div/div[3]/div/div/div/div[6]/span[2]').click()
-    await page.getByText('Desktop').click()
-    await page.getByText('Word File.doc').click()
-    await page.waitForTimeout(3000)
+    await page.locator('.rc-tree-switcher').click();
+    await page.locator('.rc-tree-switcher.rc-tree-switcher_close').first().click();
+    await page.locator('div:nth-child(6) > .rc-tree-switcher').click();
+    await page.getByRole('checkbox', { name: 'Select Word File.doc' }).click();
+    await page.getByRole('checkbox', { name: 'Select Notes' }).click();
+    expect(await page.locator('div',{name:'You have selected :'}).filter(page.locator('span',{name:'text-success'}))).toBeVisible()
+    // await page.waitForTimeout(3000)
 })
