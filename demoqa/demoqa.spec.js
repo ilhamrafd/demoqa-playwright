eb1655cd8922b5f534c5b9e16f39a549d4f8e414
ilhamrafif20 <ilham.rafif20@gmail.com>
2026-02-26 22:52:17 +0700

    adding register and login

--- demoqa/demoqa.spec.js
+++ demoqa/demoqa.spec.js
@@ -1,43 +1,24 @@
 import {test,expect} from '@playwright/test'
-test("demoqa - Text Box - Positive Case",async function ({page}) {
-    await page.goto('https://demoqa.com/text-box')
-    await page.getByPlaceholder('Full Name').fill('Ilham',{delay:2000}) // fill full name
-    await page.getByPlaceholder('name@example.com').fill('ilham@mail.com',{delay:3000}) // fill email
-    await page.getByPlaceholder('Current Address').fill('Oslo',{delay:2000}) // fill address
-    await page.locator('//*[@id="permanentAddress"]').fill('Oslo, Norway',{delay:4000}) // fill permanent address
-    await page.locator('//*[@id="submit"]').click() // click submit button
-    expect(await page.getByText('Ilham')).toBeTruthy() // validation check if text true (appear)
-    // await page.waitForTimeout(3000) 
-})
-
-test("demoqa - Text Box - Negative Case",async function ({page}) {
-    await page.goto('https://demoqa.com/text-box')
-    await page.getByPlaceholder('Full Name').fill('2008993511110001')
-    await page.getByPlaceholder('name@example.com').fill('ilham.mail.com')
-    await page.getByRole('button',{id:'submit'}).click()
-    expect(page.getByPlaceholder('name@example.com')).toContainClass('mr-sm-2 field-error form-control') // validation for email field
-    // await page.waitForTimeout(3000)
-})
-
-test("demoqa - Checkbox",async function ({page}) {
+test("demoqa - Book Store App - Register",async function ({page}) {
+    // Register
     await page.goto('https://demoqa.com/')
-    await page.getByText('Elements').click()
-    await page.getByText('Check Box').click()
-    await page.locator('.rc-tree-switcher').click();
-    await page.locator('.rc-tree-switcher.rc-tree-switcher_close').first().click();
-    await page.locator('div:nth-child(6) > .rc-tree-switcher').click();
-    await page.getByRole('checkbox', { name: 'Select Word File.doc' }).click();
-    await page.getByRole('checkbox', { name: 'Select Notes' }).click();
-    expect(await page.locator('div',{name:'You have selected :'}).filter(page.locator('span',{name:'text-success'}))).toBeVisible()
-    // await page.waitForTimeout(3000)
+    await page.getByText('Book Store Application').click()
+    await page.getByText('Login').click()
+    await page.getByRole('button',{name:'New User'}).click()
+    await page.getByPlaceholder('First Name').fill('ilham rafif',{delay:3000})
+    await page.getByPlaceholder('Last Name').fill('d')
+    await page.getByPlaceholder('UserName').fill('ilham20',{delay:3000})
+    await page.getByPlaceholder('Password').fill('!lh4mk0pl4kZ',{delay:3000})
+    await page.getByRole('button',{name:'Register'}).click()
 })
 
-test("demoqa - Radio Button",async function ({page}) {
+test.only("demoqa - Book Store App - Login",async function ({page}) {
     await page.goto('https://demoqa.com/')
-    await page.getByText('Elements').click()
-    await page.getByText('Radio Button').click()
-    await page.getByText('Yes').click()
-    await page.getByText('Impressive').click()
-    expect(await page.locator('p',{name:'You have selected '}).filter(page.locator('span',{name:'text-success'}))).toBeVisible()
-    await page.waitForTimeout(3000)
+    await page.getByText('Book Store Application').click()
+    await page.getByRole('button',{name:'Login'}).click()
+    await page.getByRole('button',{name:'New User'}).click()
+    await page.getByRole('button',{name:'Back to Login'}).click()
+    await page.getByPlaceholder('UserName').fill('ilham20',{delay:3000})
+    await page.getByPlaceholder('Password').fill('!lh4mk0pl4kZ',{delay:3000})
+    await page.getByRole('button',{name:'Login'}).click()
 })
